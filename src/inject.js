fd80de27f1335aef53cafc7d25e9bb87bb525dbf
nickvirtue <nick@mahuika.co.nz>
2017-05-14 00:03:08 +1200

    Introduce inject HOC to inect app().xxx domains etc

---
+++ src/inject.js
@@ -0,0 +1,30 @@
+import { Component } from 'react'
+import setDisplayName from 'recompose/setDisplayName'
+import wrapDisplayName from 'recompose/wrapDisplayName'
+import createEagerFactory from 'recompose/createEagerFactory'
+import fromPairs from 'lodash/fromPairs'
+
+import { app } from '@mindhive/di'
+
+const injectHOC = domains => (BaseComponent) => {
+  const factory = createEagerFactory(BaseComponent)
+
+  class Inject extends Component {
+
+    render() {
+      return factory({
+        ...this.props,
+        ...fromPairs((domains || []).map(d => [d, app()[d]])),
+      })
+    }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    return setDisplayName(wrapDisplayName(BaseComponent, 'inject'))(
+      Inject
+    )
+  }
+  return Inject
+}
+
+export default injectHOC
